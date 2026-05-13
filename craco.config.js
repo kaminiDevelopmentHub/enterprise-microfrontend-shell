47c129f605785b2c24a59713079c3f0f68c27aba
Kamini Parmar <kaminiparmar@Kaminis-MacBook-Air.local>
2026-05-13 09:54:20 +0530

    update mfa

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,28 @@
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.plugins.push(
-        new ModuleFederationPlugin({
-          name: "shell",
-          remotes: {
-            dashboard: "dashboard@http://localhost:3001/remoteEntry.js",
-          },
-          shared: {
-            react: { singleton: true },
-            "react-dom": { singleton: true },
-          },
-        })
-      );
-      return webpackConfig;
+    webpack: {
+        configure: (config) => {
+            config.plugins.push(
+                new ModuleFederationPlugin({
+                    name: "shell",
+                    remotes: {
+                        dashboard: "dashboard@http://localhost:3001/remoteEntry.js",
+                        orders: "orders@http://localhost:3002/remoteEntry.js",
+                    },
+                    shared: {
+                        // react: {
+                        //     singleton: true,
+                        //     requiredVersion: false,
+                        // },
+                        // "react-dom": {
+                        //     singleton: true,
+                        //     requiredVersion: false,
+                        // },
+                    },
+                })
+            );
+            return config;
+        },
     },
-  },
 };
